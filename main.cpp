62a888e294935dbd306efffca702b29c46a3a3db
James <thatoneddrguy@gmail.com>
2019-05-09 22:19:10 -0700

    Changed tab spacing from 2 spaces to 4 spaces

--- main.cpp
+++ main.cpp
@@ -6,26 +6,26 @@ using namespace std;
 
 int main()
 {
-  int memSize;
-  int pageSize;
-  ifstream inFile = ifstream("in1.txt");
-  ofstream outFile = ofstream("out.txt");
-  queue <int> arrivalTimes;
-  queue <int> burstTimes;
-  queue <int> memoryNeeds;
-  queue <int> processQueue;
-  LLNode* memoryMapHead = NULL;
-  double avgTurnaroundTime = 0.0;
+    int memSize;
+    int pageSize;
+    ifstream inFile = ifstream("in1.txt");
+    ofstream outFile = ofstream("out.txt");
+    queue <int> arrivalTimes;
+    queue <int> burstTimes;
+    queue <int> memoryNeeds;
+    queue <int> processQueue;
+    LLNode* memoryMapHead = NULL;
+    double avgTurnaroundTime = 0.0;
 
-  memoryMapHead = new LLNode();
+    memoryMapHead = new LLNode();
 
-  cout << "Memory size>";
-  cin >> memSize;
-  cout << "\n";
+    cout << "Memory size>";
+    cin >> memSize;
+    cout << "\n";
 
-  cout << "Page Size (1: 100, 2: 200, 3: 400)>";
-  cin >> pageSize;
+    cout << "Page Size (1: 100, 2: 200, 3: 400)>";
+    cin >> pageSize;
 
-  outFile << memSize << " " << pageSize;
-  return 0;
+    outFile << memSize << " " << pageSize;
+    return 0;
 }
